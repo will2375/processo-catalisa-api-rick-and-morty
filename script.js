0ab226a5a09ba2df8f3d80549dd9e42e63e75036
will2375 <wil.liam_benicio@hotmail.com>
2022-01-15 17:23:29 -0300

    corrigir html

--- script.js
+++ script.js
@@ -9,3 +9,27 @@ const especie3 = document.querySelector('#especie3');
 const condicao = document.querySelector('#status');
 const condicao2 = document.querySelector('#status2');
 const condicao3 = document.querySelector('#status3');
+
+gerarValorAletorio = () => {
+    return Math.floor(Math.random() * 671);
+}
+
+pegarPersonagem = () => {
+    let numeroAleatorio = gerarValorAletorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+        method:'GET',
+        headers:{
+            Accept: 'application.json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) =>{
+        imagem.src = data.image;
+        imagem.alt = data.name;
+        nomeDoPersonagem.innerHTML = data.name;
+        especie.innerHTML = data.species;
+        condicao.innerHTML = data.status;
+
+    });
+}
+
+botao.onclick = pegarPersonagem;
