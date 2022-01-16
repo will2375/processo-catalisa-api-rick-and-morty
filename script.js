fd937f19015c508256d84c96a5c7aabaea11f53b
will2375 <wil.liam_benicio@hotmail.com>
2022-01-16 14:37:28 -0300

    finalizaçao do projeto

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const imagem = document.querySelector('img');
 const imagem2 = document.querySelector('#img2');
 const imagem3 = document.querySelector('#img3');
 const botao = document.querySelector('button');
-const botao2 = document.querySelector('button');
-const botao3 = document.querySelector('button');
 const nomeDoPersonagem = document.querySelector('#nome');
 const nomeDoPersonagem2 = document.querySelector('#nome2');
 const nomeDoPersonagem3 = document.querySelector('#nome3');
@@ -14,30 +12,42 @@ const condicao = document.querySelector('#status');
 const condicao2 = document.querySelector('#status2');
 const condicao3 = document.querySelector('#status3');
 
+traduzirCondicao = (data) => {
+    if(data.status == 'unknown'){
+        return 'Não sabemos';
+    }else if(data.status == 'Alive'){
+        return 'Sim';
+    }else {
+        return 'Não. Está morto';
+    }
+}
+
 gerarValorAletorio = () => {
     return Math.floor(Math.random() * 671);
 }
 
 
 pegarPersonagem = () => {
-    let numeroAleatorio = gerarValorAletorio();
-    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
-        method:'GET',
-        headers:{
-            Accept: 'application.json',
-            "Content-type": 'application/json'
-        }
-    }).then((response) => response.json()).then((data) =>{
-        imagem.src = data.image;
-        imagem.alt = data.name;
-        nomeDoPersonagem.innerHTML = data.name;
-        especie.innerHTML = data.species;
-        condicao.innerHTML = data.status;
 
-    });
- 
-
-}
+        let numeroAleatorio = gerarValorAletorio();
+        return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+            method:'GET',
+            headers:{
+                Accept: 'application.json',
+                "Content-type": 'application/json'
+            }
+        }).then((response) => response.json()).then((data) =>{
+            imagem.src = data.image;
+            imagem.alt = data.name;
+            nomeDoPersonagem.innerHTML = data.name;
+            especie.innerHTML = data.species;
+            condicao.innerHTML = data.status;
+            condicao.innerHTML = traduzirCondicao(data);
+    
+        });
+     
+    
+    }
 gerarValorAletorio2 = () => {
     return Math.floor(Math.random() * 671);
 }
@@ -56,6 +66,7 @@ pegarPersonagem2 = () => {
         nomeDoPersonagem2.innerHTML = data.name;
         especie2.innerHTML = data.species;
         condicao2.innerHTML = data.status;
+        condicao2.innerHTML = traduzirCondicao(data);
 
     });
 }
@@ -76,7 +87,15 @@ pegarPersonagem3 = () => {
         nomeDoPersonagem3.innerHTML = data.name;
         especie3.innerHTML = data.species;
         condicao3.innerHTML = data.status;
+        condicao3.innerHTML = traduzirCondicao(data);
 
     });
 }
-botao.onclick = pegarPersonagem('img'), pegarPersonagem2('#img2'), pegarPersonagem3('img3');
+
+executaOs3 = function(){
+    pegarPersonagem();
+    pegarPersonagem2();
+    pegarPersonagem3();        
+ };
+
+botao.onclick = executaOs3
